e3dba55289dd43a96c8f79f2d142ecff112bf63a
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-11-24 17:20:59 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""Main story.
+"""
+import os
+import sys
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.append('storybuilder')
+from storybuilder.builder.world import World
+from config import PERSONS, CHARAS, STAGES, DAYS, TIMES, ITEMS, WORDS
+
+## main
+def world():
+    """Create a world.
+    """
+    w = World("")
+    w.set_db(PERSONS, CHARAS,
+            STAGES, DAYS, TIMES,
+            ITEMS,
+            WORDS)
+    return w
+
+def story(w: World):
+    return w.story("Sample",
+            )
+
+def main(): # pragma: no cover
+    w = world()
+    return w.build(story(w))
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main())
+
