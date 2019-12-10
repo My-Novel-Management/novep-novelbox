40385e87d1dc17e5ad0ef9bff5accdbd6e3d48da
N.T.WORKS <nagisc007@yahoo.co.jp>
2019-12-10 11:07:13 +0900

    Added: demo (#16)
    * Added: demo
    
    * Fixed: typo (forget adding)

--- snow/demo/demo.py
+++ snow/demo/demo.py
@@ -12,10 +12,29 @@ W = Writer
 
 
 ## scenes
-
+def sc_dontgoout(w: World):
+    yu, sa = W(w.yuki), W(w.sabu)
+    masa = W(w.masa)
+    return w.scene("行かないで下さい", "彼女は何故か三郎が町に降りるのを止めた",
+            yu.talk("お願いですから"),
+            yu.do("引っ張る"),
+            sa.talk("何故だ？　最近妙だぞ"),
+            yu.talk("春が、近いのです"),
+            sa.talk("だから春になれば山を降りて色々なところにお前を連れて行ってやりたい"),
+            yu.refuse(),
+            sa.talk("何故だ？"),
+            sa.go("外に出て"),
+            yu.tears(),
+            w.br(),
+            masa.come(),
+            masa.look("冷たくなった$n_sabuを"),
+            masa.feel("冷たいと"),
+            masa.look(doing="まるで生きているように綺麗な肌をして笑っていた"),
+            )
 
 ## episode
 def ep_demo(w: World):
     return w.episode("Demo", "demo story",
+            sc_dontgoout(w),
             )
 
